8af4a1d6f9f4ccd8de21b225127a192c08212a70
Ben Chapman <0xbenc@gmail.com>
2024-10-30 11:56:13 -0700

    added WIP ubuntu startup

---
+++ setup/ubuntu_startup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update machine
+sudo apt-get update
+
+# Install basic stuff
+sudo apt install -y figlet git-lfs inotify-tools ncdu nload tmux htop
+
+# Install fastfetch dependencies
+sudo apt install -y git cmake pkg-config g++
+
+# Fastfetch compile and install
+git clone https://github.com/LinusDierheimer/fastfetch.git
+cd fastfetch
+mkdir -p build
+cd build
+cmake ..
+make
+sudo make install
+cd ../..
+
+# Tmux setup
+tmux new -d -s "admin"
+tmux new-window -t "admin" -n "cpu" "htop"
+tmux new-window -t "admin" -n "disk" "ncdu"
+tmux new-window -t "admin" -n "network" "nload"
+tmux new-window -t "admin" -n "about" "fastfetch; read -n1 -r -p 'Press any key to close...'"
+
+# NVM setup
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
